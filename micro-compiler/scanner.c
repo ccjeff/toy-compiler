9783e29123086848ea79c76ed1dd1934dc8e7500
ccjeff <jefferycao.czj@gmail.com>
2021-03-03 00:24:43 +0800

    added scanner

---
+++ micro-compiler/scanner.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "scanner.h"
+
+extern int yylex();
+extern int yylineno;
+extern char* yytext;
+
+int main(void){
+
+	int ntoken = yylex();
+	while(ntoken) {
+		printf("%d\n", ntoken);
+		ntoken = yylex();
+	}
+	return 0;
+}
---
+++ micro-compiler/scanner.h
@@ -0,0 +1,19 @@
+#define BEGINN 1
+#define END   2
+#define READ  3
+#define WRITE 4
+#define ID    5
+#define INTLITERAL 6
+#define LPAREN 7
+#define RPAREN 8
+#define SEMICOLON 9
+#define COMMA 10
+#define ASSIGNOP 11
+#define PLUOP 12
+#define MINUSOP 13
+#define SCANEOF 14
+
+
+
+
+
